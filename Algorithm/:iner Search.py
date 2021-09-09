e429f16631ed8183b0fcc371a9c96699ddd12839
yj-leee <yj.leee1010@gmail.com>
2021-09-09 17:52:13 +0900

    liner_serarch
    선형 탐색!

--- Algorithm/:iner Search.py
+++ Algorithm/:iner Search.py
@@ -8,3 +8,16 @@
 
 element가 some_list에 존재하지 않는 값이면 None을 리턴해주세요.
 """
+
+
+def linear_search(element, some_list):
+    for idx in range(len(some_list)):
+        if some_list[idx] == element:
+            return idx
+
+
+print(linear_search(2, [2, 3, 5, 7, 11]))
+print(linear_search(0, [2, 3, 5, 7, 11]))
+print(linear_search(5, [2, 3, 5, 7, 11]))
+print(linear_search(3, [2, 3, 5, 7, 11]))
+print(linear_search(11, [2, 3, 5, 7, 11]))
