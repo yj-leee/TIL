f164a6d8a05c18eac812ec2ab2c34b47f5a68f7e
yj-leee <yj.leee1010@gmail.com>
2021-11-07 17:07:31 +0900

    fix

--- Algorithm/BFS/02.py
+++ Algorithm/BFS/02.py
@@ -12,7 +12,7 @@ for i in range(n):
         # 해당 위치에 바이러스가 존재하는 경우
         if graph[i][j] != 0:
             # 바이러스 종류, 시간, 위치 삽입
-            data.append(graph[i][j], 0, i, j)
+            data.append((graph[i][j], 0, i, j))
 
 
 # 정렬 이후에 큐로 옮기기 (낮은 번호의 바이러스가 먼저 증식하므로)
@@ -27,7 +27,7 @@ dy = [0, 1, 0, -1]
 
 # 너비 우선 탐색(BFS) 진행
 while q:
-    virus, s, x, y = q.poplift()
+    virus, s, x, y = q.popleft()
     # 정확히 s초가 지나거나, 큐가 빌 때까지 반복
     if s == target_s:
         break
@@ -41,6 +41,6 @@ while q:
             # 아직 방문하지 않은 위치라면, 그 위치에 바이러스 넣기
             if graph[nx][ny] == 0:
                 graph[nx][ny] = virus
-                q.append(virus, s + 1, nx, ny)
+                q.append((virus, s + 1, nx, ny))
 
 print(graph[target_x - 1][target_y - 1])
