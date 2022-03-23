a543e9fe537efecd7ef7ee9e499b33d056186382
yj-leee <yj.leee1010@gmail.com>
2022-03-23 12:14:50 +0900

    add

--- Algorithm/Dynamic/05.py
+++ Algorithm/Dynamic/05.py
@@ -5,14 +5,15 @@ dp = []
 for _ in range(n):
     dp.append(list(map(int, input().split())))
 
+
 for i in range(1, n):
     for j in range(i + 1):
-
+        # 왼쪽 위에서 내려오는 경우
         if j == 0:
             up_left = 0
         else:
             up_left = dp[i - 1][j - 1]
-
+        # 바로 위에서 내려오는 경우
         if j == i:
             up = 0
         else:
