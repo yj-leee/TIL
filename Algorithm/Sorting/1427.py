fb9f10bd31233a923b92e3043a13b90b43867c4b
yj-leee <yj.leee1010@gmail.com>
2022-07-02 13:39:17 +0900

    add

---
+++ Algorithm/Sorting/1427.py
@@ -0,0 +1,24 @@
+# n = int(input())
+
+# data = []
+# for i in str(n):
+#     data.append(i)
+
+# result = []
+# for j in data:
+#     result.append(int(j))
+
+# result.sort(reverse=True)
+
+
+# response = ''
+# for k in result:
+#     response += str(k)
+# print(response)
+
+
+a = list(input())
+print(a)
+a.sort(reverse = True)
+print(a)
+print(''.join(a)) 
